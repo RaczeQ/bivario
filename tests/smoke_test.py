2a554d9cf8dda24bbf73318b9f217c6aae0a6c7d
Kamil Raczycki <raczyckikamil@gmail.com>
2025-10-21 22:05:08 +0200

    chore: update smoke test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,16 +1,14 @@
 """Check that basic features work."""
 
 if __name__ == "__main__":
-    from bivario.cmap import bivariate_from_params
+    from bivario import get_bivariate_cmap
     from bivario.example_data import nyc_bike_trips
 
     dataset = nyc_bike_trips()
 
-    cmap = bivariate_from_params(
-        values_a=dataset["morning_starts"],
-        values_b=dataset["morning_ends"],
-        params="electric_neon",
-    )
+    bv_cmap = get_bivariate_cmap()
+
+    cmap = bv_cmap(values_a=dataset["morning_starts"], values_b=dataset["morning_ends"])
 
     if cmap.shape == (1569, 3):
         print("Smoke test succeeded")
