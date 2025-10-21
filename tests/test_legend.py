0e0456ae6a905ced7b9362f325faf4b8ec6320b2
Kamil Raczycki <raczyckikamil@gmail.com>
2025-10-21 23:29:12 +0200

    fix: update test_legend.py

--- tests/test_legend.py
+++ tests/test_legend.py
@@ -3,6 +3,6 @@
 
 def test_plot_bivariate_legend() -> None:
     """Test that plot_bivariate_legend can be run without error."""
-    from bivario._legend import plot_bivariate_legend
+    from bivario.legend import plot_bivariate_legend
 
     plot_bivariate_legend(values_a=[0, 1], values_b=[0, 1])
