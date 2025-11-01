c0806cee0fe2bd799dadf24b6d532caea8aaa0cf
RaczeQ <17250607+RaczeQ@users.noreply.github.com>
2025-11-01 19:37:40 +0000

    chore(CI/CD): bump version 0.3.0 -> 0.4.0

--- bivario/__init__.py
+++ bivario/__init__.py
@@ -16,7 +16,7 @@ from bivario.legend import plot_bivariate_legend
 from bivario.lonboard import viz_bivariate_data
 
 __app_name__ = "bivario"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "AccentsBivariateColourmap",
